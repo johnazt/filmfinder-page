95d44c0189f5bdf10940b5798d32b56698380668
John Asto <john.azt.dev@gmail.com>
2023-04-07 17:57:06 -0400

    add responsive carousel

--- app/components/PopularMovies.tsx
+++ app/components/PopularMovies.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import { Movie } from "../interfaces/movie";
 import styles from "../../styles/popular.module.css";
+import Link from "next/link";
 
 interface PopularMoviesProps {
   allPopularMovies: Movie[];
@@ -18,16 +19,18 @@ const PopularMovies = ({ allPopularMovies, baseUrl }: PopularMoviesProps) => {
             key={m.id}
             className="border rounded-lg overflow-hidden min-h-[400px]"
           >
-            <Image
-              src={`${baseUrl}original${m.poster_path}`}
-              alt={m.original_language}
-              height={200}
-              width={200}
-              className={`${styles.img}`}
-            />
-            <div className="p-4">
-              <h3 className="font-bold text-lg text-center">{m.title}</h3>
-            </div>
+            <Link href="/">
+              <Image
+                src={`${baseUrl}original${m.poster_path}`}
+                alt={m.original_language}
+                height={200}
+                width={200}
+                className={`${styles.img}`}
+              />
+              <div className="p-4">
+                <h3 className="font-bold text-lg text-center">{m.title}</h3>
+              </div>
+            </Link>
           </div>
         ))}
       </div>
--- app/components/ReleaseMovies.tsx
+++ app/components/ReleaseMovies.tsx
@@ -1,7 +1,10 @@
+"use client";
 import React from "react";
 import { Movie } from "../interfaces/movie";
 import Link from "next/link";
 import Image from "next/image";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 interface ReleaseMoviesProps {
   allReleaseMovies: Movie[];
@@ -11,17 +14,26 @@ interface ReleaseMoviesProps {
 const ReleaseMovies = ({ allReleaseMovies, baseUrl }: ReleaseMoviesProps) => {
   return (
     <div>
-      {allReleaseMovies.map((m) => (
-        <Link key={m.id} href="/">
-          <Image
-            src={`${baseUrl}original${m.backdrop_path}`}
-            alt={m.original_title}
-            height={200}
-            width={200}
-          />
-          <p>{m.title}</p>
-        </Link>
-      ))}
+      <Carousel
+        showArrows={true}
+        showThumbs={false}
+        showIndicators={false}
+        infiniteLoop={true}
+        showStatus={false}
+        autoPlay
+      >
+        {allReleaseMovies.map((m) => (
+          <Link key={m.id} href="/">
+            <Image
+              src={`${baseUrl}original${m.backdrop_path}`}
+              alt={m.original_title}
+              height={0}
+              width={500}
+            />
+            <p>{m.title}</p>
+          </Link>
+        ))}
+      </Carousel>
     </div>
   );
 };
