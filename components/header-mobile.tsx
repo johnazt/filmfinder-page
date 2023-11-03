e081e216eb40096d3eaa388922f7c94a7f3e1dcd
John Asto <john.azt.dev@gmail.com>
2023-11-03 20:59:47 -0300

    #2 add header-mobile setting

--- components/header-mobile.tsx
+++ components/header-mobile.tsx
@@ -6,9 +6,7 @@ const HeaderMobile = () => {
       <Link href="/">
         <span className="text-2xl font-bold text-red-500">LastMovies</span>
       </Link>
-      <button>
-        <HambugerIcon />
-      </button>
+      <HambugerIcon />
     </nav>
   );
 };
@@ -17,17 +15,19 @@ export default HeaderMobile;
 
 export const HambugerIcon = () => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="16"
-      viewBox="0 0 24 16"
-      fill="none"
-    >
-      <path
-        d="M0 16H24V13.3333H0V16ZM0 9.33333H24V6.66667H0V9.33333ZM0 0V2.66667H24V0H0Z"
-        fill="white"
-      />
-    </svg>
+    <button className="pointer-events-auto absolute right-5 top-7 z-30">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="16"
+        viewBox="0 0 24 16"
+        fill="none"
+      >
+        <path
+          d="M0 16H24V13.3333H0V16ZM0 9.33333H24V6.66667H0V9.33333ZM0 0V2.66667H24V0H0Z"
+          fill="white"
+        />
+      </svg>
+    </button>
   );
 };
